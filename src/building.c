fce74c458a03dea65e826e6076e8038c02e38de5
Brychlikov <brychlikow@gmail.com>
2021-01-24 12:33:05 +0100

    track assets

---
+++ src/building.c
@@ -0,0 +1,5 @@
+//
+// Created by brych on 24/01/2021.
+//
+
+#include "building.h"
---
+++ src/building.h
@@ -0,0 +1,8 @@
+//
+// Created by brych on 24/01/2021.
+//
+
+#ifndef _RTS_BUILDING_H
+#define _RTS_BUILDING_H
+
+#endif //_RTS_BUILDING_H
